fee76d8d4bf70c5a46026d3d5cf086fb8de7a304
ruoyanchen-design <ruoyan85@stanford.edu>
2021-10-25 10:52:09 -0700

    Add pendulum.py

--- gcml/envs/pendulum/pendulum.py
+++ gcml/envs/pendulum/pendulum.py
@@ -1,2 +1,11 @@
+import numpy as np
+import gym
+from gcml.envs.base import MetaGoalReachingEnv
+from gcml.envs.pendulum.pendulum_base import PendulumEnv
+
+class MetaPendulumEnv(MetaGoalReachingEnv):
+    def __init__(self):
+        base_env = PendulumEnv()
+        
 
 
