aa2ff1d3508baad1567450e8777d4c2e98aa489f
Yunfan Jiang <yunfanjyf@gmail.com>
2021-11-02 09:47:34 -0700

    fix API for sample trajectory | add help fns

--- gcml/algorithms/common/utils.py
+++ gcml/algorithms/common/utils.py
@@ -1,3 +1,8 @@
+from typing import Dict
+
+import torch
+import numpy as np
+
 from ...models import MetaGoalReachAgent
 from ...envs import MetaGoalReachingEnv
 from .constants import Trajectory
@@ -5,6 +10,7 @@ from .constants import Trajectory
 
 def sample_trajectory(
         agent: MetaGoalReachAgent,
+        agent_parameters: Dict[str, torch.Tensor],
         env: MetaGoalReachingEnv,
         greedy: bool,
         exploration_coeff: float,
@@ -17,3 +23,16 @@ def evaluate_batch_trajectories(
         goal_threshold,
 ):
     raise NotImplementedError
+
+
+def np_dict_to_tensor_dict(np_dict: dict):
+    tensor_dict = {}
+    for k, v in np_dict.items():
+        if isinstance(v, dict):
+            sub_tensor_dict = np_dict_to_tensor_dict(v)
+            tensor_dict[k] = sub_tensor_dict
+        elif isinstance(v, np.ndarray):
+            tensor_dict[k] = torch.from_numpy(v)
+        else:
+            raise ValueError(f'Unknown type {type(v)}')
+    return tensor_dict
