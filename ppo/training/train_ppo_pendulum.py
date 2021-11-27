c72779de76490c026f90d47d308a1199d3db1159
ruoyanchen-design <ruoyan85@stanford.edu>
2021-11-27 14:04:47 -0800

    update train_ppo_pendulum.py

--- ppo/training/train_ppo_pendulum.py
+++ ppo/training/train_ppo_pendulum.py
@@ -10,8 +10,7 @@ from stable_baselines3.ppo import MultiInputPolicy
 from gcml.envs.pendulum.metric import metric_fn
 
 
-def evaluate(model, goal, num_eval_episodes=100, deterministic=True):
-    env = model.get_env()
+def evaluate(model, env, num_eval_episodes=100, deterministic=True):
     all_episode_distances = []
 
     for i in range(num_eval_episodes):
@@ -21,8 +20,7 @@ def evaluate(model, goal, num_eval_episodes=100, deterministic=True):
             # _states are only useful when using LSTM policies
             action, _states = model.predict(obs, deterministic=deterministic)
             obs, reward, done, info = env.step(action)
-
-        distance = metric_fn([obs["achieved_state_goal"]], [goal])
+        distance = metric_fn([obs["achieved_state_goal"]], [env.env._goal])
         all_episode_distances.append(distance)
 
     mean_episode_distance = np.mean(all_episode_distances)
@@ -32,31 +30,14 @@ def evaluate(model, goal, num_eval_episodes=100, deterministic=True):
 
 if __name__ == "__main__":
     env = PendulumWrapper(PendulumEnv(goal_threshold=0.05), max_episode_steps=100)
-    goal = env.env._goal
     model = PPO(MultiInputPolicy, env, verbose=0)
 
     # Random Agent, before training
-    mean_episode_distance, std_episode_distance = evaluate(model, goal, num_eval_episodes=100, deterministic=True)
+    mean_episode_distance, std_episode_distance = evaluate(model, env, num_eval_episodes=100, deterministic=True)
     print(f"Before training: mean_episode_distance = {mean_episode_distance:.2f} +/- {std_episode_distance:.2f}")
 
     # Train the agent for 10000 steps, and evaluate the trained agent
     model.learn(total_timesteps=10000)
-    mean_episode_distance,  std_episode_distance = evaluate(model, goal, num_eval_episodes=100, deterministic=True)
+    mean_episode_distance,  std_episode_distance = evaluate(model, env, num_eval_episodes=100, deterministic=True)
     print(f"After training: mean_episode_distance = {mean_episode_distance:.2f} +/- {std_episode_distance:.2f}")
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
 
